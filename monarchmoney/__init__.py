4cd447859787f15728752afe14835a1379fef72c
Tommy Zed <dr.tommy@gmail.com>
2026-06-23 22:47:21 +0200

    feat: export exception classes from the package initialization module

--- monarchmoney/__init__.py
+++ monarchmoney/__init__.py
@@ -5,8 +5,11 @@ A Python API for interacting with MonarchMoney.
 """
 
 from .monarchmoney import (
+    LoginFailedException,
     MonarchMoneyEndpoints,
     MonarchMoney,
+    RequireMFAException,
+    RequestFailedException,
 )
 
 __version__ = "1.1.0"
